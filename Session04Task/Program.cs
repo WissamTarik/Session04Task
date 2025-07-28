6a45afab559ca1cae9713c1253932f9d2c4f9b4d
WissamTarik <www.wissamtarik2021@gmail.com>
2025-07-28 21:06:25 +0300

    Video01

--- Session04Task/Program.cs
+++ Session04Task/Program.cs
@@ -1,9 +1,32 @@
 ﻿namespace Session04Task
 {
+
+    #region Video01 Revision
+        /*What can be written inside namespace? 
+         * 1.struct
+         * 2.enum
+         * class
+         * inteface
+         
+         */
+    #endregion
     internal class Program
     {
         static void Main(string[] args)
         {
+            #region Video01 Revision
+            //Interface:Is a code contract between developer who write it and
+            //developer who will implement it
+            //Interface is a reference type
+            /*What can be written inside interface?
+              1.Signature of property
+              2.Signature of method
+              3.Default fully implemented method
+             
+             */
+            //Shallow copy :Copy the identity(Address)
+            //Deep copy:Copy the object state(data) and generate new identity by clone method
+            #endregion
         }
     }
 }
