878dbbb342cac9be3d2f45d062f6987b1858af88
WissamTarik <www.wissamtarik2021@gmail.com>
2025-07-29 16:41:42 +0300

    FirstProject Q3

--- Assignment/FirstProject/Duration.cs
+++ Assignment/FirstProject/Duration.cs
@@ -38,5 +38,77 @@ namespace Assignment.FirstProject
             return $" Hours: {Hours}, Minutes :{Minutes}, Seconds :{Seconds} ";
         }
 
+        public static Duration operator +(Duration left, Duration right)
+        {
+
+            return new Duration(left.Hours + right.Hours, left.Minutes + right.Minutes, left.Seconds + right.Seconds);
+        }
+        public static Duration operator +(Duration left, int right)
+        {
+            int TotalSeconds = left.Hours * 3600 + left.Minutes * 60 + left.Seconds + right;
+            int hours = TotalSeconds / 3600;
+            int minutes= (TotalSeconds % 3600) / 60;
+            int seconds = TotalSeconds % 60; 
+
+            return new Duration(hours, minutes, seconds);
+        }
+        public static Duration operator +(int left, Duration right)
+        {
+            return  right + left;
+        }
+        public static Duration operator -(Duration left,Duration right)
+        {
+            int totalSecondsLeft = left.Hours * 3600 + left.Minutes * 60 + left.Seconds;
+            int totalRightTotal = right.Hours * 3600 + left.Minutes * 60 + left.Seconds;
+            int difference=totalSecondsLeft-totalRightTotal;
+            if (totalSecondsLeft < totalRightTotal)
+            {
+                difference = 0;
+                Console.WriteLine("Left hand side is smaller than right hand side");
+            }
+            int HoursTotal = difference/3600;
+            int MinutesTotal = (difference%3600)/60;
+            int SecondsTotal = difference%60;
+          
+            return new Duration(HoursTotal, MinutesTotal, SecondsTotal);
+        }
+       public static Duration operator ++(Duration duration)
+        {
+            int total = duration.Minutes * 60 + duration.Hours * 3600 + duration.Seconds;
+            total += 60;
+            int TotalHours = total / 3600;
+            int TotalMinutes = (total % 3600) / 60;
+            int TotalSeconds = total % 60;
+
+            return new Duration(TotalHours, TotalMinutes, TotalSeconds);
+        
+        }
+       public static Duration operator --(Duration duration)
+        {
+            int total = duration.Minutes * 60 + duration.Hours * 3600 + duration.Seconds;
+            total -= 60;
+            if (total<0)
+            {
+                total = 0;
+            }
+
+            int TotalHours = total / 3600;
+            int TotalMinutes = (total % 3600) / 60;
+            int TotalSeconds = total % 60;
+            
+            return new Duration(TotalHours, TotalMinutes, TotalSeconds);
+        }
+        public static bool operator >(Duration left, Duration right) {
+
+            int LeftTotal = left.Hours * 3600 + left.Minutes * 60 + left.Seconds;
+            int RightTotal = right.Hours * 3600 + right.Minutes * 60 + right.Seconds;
+            return LeftTotal > RightTotal;
+        }
+        public static bool operator <(Duration left, Duration right) {
+
+            int LeftTotal = left.Hours * 3600 + left.Minutes * 60 + left.Seconds;
+            int RightTotal = right.Hours * 3600 + right.Minutes * 60 + right.Seconds;
+            return LeftTotal < RightTotal;
+        }
     }
 }
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -8,15 +8,45 @@ namespace Assignment
         {
             //Duration D1 = new Duration(1, 10, 15);
             //Console.WriteLine(D1.ToString());
+            //Console.WriteLine("=======================");
+            //Console.WriteLine();
+            ////Duration D1 = new Duration(3600);
+            ////Console.WriteLine(D1.ToString());
 
-            //Duration D1 = new Duration(3600);
-            //Console.WriteLine(D1.ToString());
+            //Duration D2 = new Duration(7800);
+            //Console.WriteLine(D2.ToString());
+            //Console.WriteLine("===========================");
+            //Console.WriteLine();
 
-            //Duration D1 = new Duration(7800);
-            //Console.WriteLine(D1.ToString());
+            ////Duration D1 = new Duration(666);
+            ////Console.WriteLine(D1.ToString());
 
-            //Duration D1 = new Duration(666);
-            //Console.WriteLine(D1.ToString());
+            ////Duration D3 = D1 + D2;
+            ////Console.Write("+ Operator:");
+            ////Console.WriteLine(D3);
+
+            ////Duration D3 = D1 + 7800;
+            ////Console.WriteLine(D3);
+            ////D3 = 666 + D3;
+            ////Console.WriteLine(D3);
+
+            ////Duration D3;
+            //////D3 = ++D1;
+            //////Console.WriteLine(D3.ToString());
+            ////D3 = --D2;
+            ////Console.WriteLine(D3.ToString());
+
+            //// D1= D1 - D2;
+            ////Console.Write("- Operator:");
+            ////Console.WriteLine(D1);
+            //if (D1 > D2)
+            //{
+            //    Console.WriteLine("D1 is greater than D2");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("D1 isn't greater than D2");
+            //}
         }
     }
 }
