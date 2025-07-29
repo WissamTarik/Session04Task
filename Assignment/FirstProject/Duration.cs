10b88a3a643bfd4ca887428ae0b68bce0b8db4ab
WissamTarik <www.wissamtarik2021@gmail.com>
2025-07-29 15:28:21 +0300

    First project Q1

---
+++ Assignment/FirstProject/Duration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.FirstProject
+{
+    internal class Duration
+    {
+        public int Hours { get; set; }
+        public int Minutes { get; set; }
+        public int Seconds { get; set; }
+
+        public Duration(int hours, int minutes, int seconds) :this(seconds)
+        {
+            Hours = hours; 
+            Minutes=minutes;
+        }
+        public Duration(int seconds)
+        {
+            Seconds = seconds;
+        }
+      
+    }
+}
